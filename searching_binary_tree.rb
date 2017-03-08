57c165e156af84669ae4231c44d6e04a761cc3a7
hermaneric5605 <herman7279@gmail.com>
2017-03-08 16:01:46 -0500

    seconf commit

--- searching_binary_tree.rb
+++ searching_binary_tree.rb
@@ -1,4 +1,12 @@
 class Node
 	attr_accessor :value, :parent, :left_child, :right_child
+	def initialize(value, parent = nil, left_child = nil, right_child = nil)
+		@value = value
+		@parent = parent
+		@left_child = left_child
+		@right_child = right_child
+	end
 
 end
+	#write a method build_tree which takes an array of data and turns it into a binary tree full of Node objects appropriately placed
+	def build_tree(array=[])
